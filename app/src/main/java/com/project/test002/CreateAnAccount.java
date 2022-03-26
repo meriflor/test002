299f4b0f0e079eb81f73d489731911f90a2eb856
prodeveloper-harris <harrisrevelo540@gmail.com>
2022-03-26 13:37:30 +0800

    setting some conditions for log in

--- app/src/main/java/com/project/test002/CreateAnAccount.java
+++ app/src/main/java/com/project/test002/CreateAnAccount.java
@@ -4,19 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CreateAnAccount extends AppCompatActivity {
 
     private TextView signIn;
+    private EditText name;
+    private EditText signupEmail;
+    private EditText signupPass;
+    private EditText retypePass;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_an_account);
 
+
         signIn = findViewById(R.id.tv_signin);
+        name = findViewById(R.id.et_fullName);
+        signupEmail = findViewById(R.id.et_email);
+        signupPass = findViewById(R.id.et_password);
+        retypePass = findViewById(R.id.et_retype_password);
+        signupButton = findViewById(R.id.btn_signup);
+
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +40,49 @@ public class CreateAnAccount extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if(validateData()) {
+                    Intent intent = new Intent(CreateAnAccount.this, SignIn.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
+    }
+    private boolean validateData(){
+        if(name.getText().toString().isEmpty()){
+            name.setError("Enter your name");
+            return false;
+        }
+        if(signupEmail.getText().toString().isEmpty()) {
+            signupEmail.setError(" Enter Email address");
+        }
+            if(!Patterns.EMAIL_ADDRESS.matcher(signupEmail.getText().toString()).matches()){
+                signupEmail.setError(" Enter valid email address");
+                return false;
+            }
+
+
+        if(signupPass.getText().toString().isEmpty()){
+            signupPass.setError("Enter password");
+            return false;
+
+        }
+        if(signupPass.getText().toString().length()<8){
+            signupPass.setError("Password is too weak.");
+            return false;
+
+        }
+        if (!retypePass.getText().toString().equals(signupPass.getText().toString())){
+            retypePass.setError("Password does not match. Try again!" +
+                    "");
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/project/test002/SignIn.java
+++ app/src/main/java/com/project/test002/SignIn.java
@@ -4,26 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
 
     private Button signIn;
-
+    private TextView signUp;
+    private EditText loginEmail;
+    private EditText loginPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
         signIn = findViewById(R.id.btn_signin);
-
+        loginEmail= findViewById(R.id.et_email);
+        loginPass= findViewById(R.id.et_password);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+            if(validateData()){
+
+                    Intent intent = new Intent(SignIn.this, SignUp.class);
+                startActivity(intent);
+            }
+
+
+
+            }
+        });
+
+
+        signUp = findViewById(R.id.tv_signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent = new Intent(SignIn.this, SignUp.class);
                 startActivity(intent);
             }
         });
+
+    }
+
+    private boolean validateData(){
+
+       /* if(loginEmail.getText().toString().isEmpty()){
+            loginEmail.setError("Enter email address");
+            return false;
+        }*/
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(loginEmail.getText().toString()).matches()){
+            loginEmail.setError("Invalid Email address");
+            return false;
+        }
+        if(loginPass.getText().toString().isEmpty()){
+           loginPass.setError("Enter password");
+           return false;
+        }
+        return true;
     }
 }
